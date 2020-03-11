229b505206bb4ed84aa006ca7015d2fceab21cbf
Joe Sturzenegger <joesturz84@gmail.com>
2020-03-11 16:36:33 -0600

    added install oh-my-zsh to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 cp bash_profile ~/.bash_profile
 cp bullet-train-dark-core.zsh-theme ~/.oh-my-zsh/custom/themes/bullet-train-dark-core.zsh-theme
 cp Themes/NeonNight.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/NeonNight.xccolortheme
 
+brew cask install shiftit
+brew install archey
