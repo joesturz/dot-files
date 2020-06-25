d1a7647fa88ad0a6b15f88602eee37c45c22def5
Joe Sturzenegger <joesturz84@gmail.com>
2020-06-25 10:59:55 -0600

    add script for concatination of audiobook files

---
+++ helpful_scripts/concat_audio_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ----- Notes -----
+# checkout https://helpful.wiki/ffmpeg/ for more ffmpeg helps
+# 
+# Remove all spaces and unusual characters from the file names
+# Remove all spaces and unusual characters from the current folder name
+
+NAME=${PWD##*/}
+
+for audiobook in *.mp3; do
+  ffmpeg -i ${audiobook} -c:a aac -c:v copy ${audiobook:r}.m4a;
+done
+mkdir mp3
+mv *.mp3 mp3
+ls -1 *.m4a | sed 's/^/file /g' > 'files.txt'
+
+ffmpeg -f 'concat' -i 'files.txt' -c copy "${NAME}.m4a"
+mkdir m4a
+mv *.mp3.m4a m4a
