b97dbfec0560d21a0eac6ab00bd82b8892a07f0b
Joe Sturzenegger <joesturz84@gmail.com>
2022-05-04 15:53:12 -0600

    add date to file name

--- helpful_scripts/get_movie_list.sh
+++ helpful_scripts/get_movie_list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-{ find '/Volumes/Movies/Movies' -name '*.m4v' -print0 -o -name '*.mp4' -print0 -o -name '*.mov' -print0 -o -name '*.mkv' -print0 -o -name '*.avi' -type f | xargs -0 basename -a; find '/Volumes/Movies/R Rated Movies/Movies' -name '*.m4v' -print0 -o -name '*.mp4' -print0 -o -name '*.mov' -print0 -o -name '*.mkv' -print0 -o -name '*.avi' -type f | xargs -0 basename -a;} > ~/Desktop/list_of_movies.txt
+now=$(date +"%m_%d_%Y")
+{ find '/Volumes/Movies/Movies' -name '*.m4v' -print0 -o -name '*.mp4' -print0 -o -name '*.mov' -print0 -o -name '*.mkv' -print0 -o -name '*.avi' -type f | xargs -0 basename -a; find '/Volumes/Movies/R Rated Movies/Movies' -name '*.m4v' -print0 -o -name '*.mp4' -print0 -o -name '*.mov' -print0 -o -name '*.mkv' -print0 -o -name '*.avi' -type f | xargs -0 basename -a;} | sort > ~/Desktop/list_of_movies_${now}.txt
