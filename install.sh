b0fd2e0c36901eea41b6db0476aebb1957553d0d
Joe Sturzenegger <joesturz84@gmail.com>
2022-07-12 17:10:48 -0600

    update install script with apps and instructions

--- install.sh
+++ install.sh
@@ -1,11 +1,46 @@
-#!/bin/bash
+#!/bin/sh
+echo install the following from the app store
+echo AppStore apps
+echo Amphetamine
+echo AppCleaner
+echo DaisyDisk
+echo Magnet
+echo Meeter
+echo Pixelmator Pro
+
+echo install these apps from the following links
+# Other Apps
+echo "https://github.com/DanielStormApps/Fanny"
+echo "https://plexamp.com/#downloads"
+echo "https://github.com/MonitorControl/MonitorControl#readme"
+
+echo find you Xcode version here
+echo "https://developer.apple.com/download/all/"
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 cp bash_profile ~/.bash_profile
-cp bullet-train-dark-core.zsh-theme ~/.oh-my-zsh/custom/themes/bullet-train-dark-core.zsh-theme
-cp Themes/NeonNight.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/NeonNight.xccolortheme
+cp Themes/oh_my_zsh/bullet-train-dark-core.zsh-theme ~/.oh-my-zsh/custom/themes/bullet-train-dark-core.zsh-theme
+cp Themes/Xcode/NeonNight.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/NeonNight.xccolortheme
 
-brew cask install shiftit
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install archey
 brew install htop
+brew install bat
+brew install ffmpeg
+brew install tldr
+brew install tmux
+brew install wget
+brew install carthage
+
+# cask install
+brew cask install shiftit
+brew cask install dozer
+brew cask install iina
+brew cask install vscodium
+brew cask install proxyman
+brew cask install sourcetree
+brew cask install telegram
+brew cask install bitwarden
+brew cask install slack
